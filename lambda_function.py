19ec1e5d9014f581456689049e08d4d69e5c96df
Hadronlink <hadronlink@hadronlink.com>
2024-11-26 12:51:06 -0500

    added files

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+import pandas as pd
+
+def lambda_handler(event, context):
+    my_list = [1, 2, 3, 4, 5]
+    print(my_list)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
