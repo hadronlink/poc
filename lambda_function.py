73272badeaf5f7a3e071a0498fcb00bdb44a0120
Hadronlink <hadronlink@hadronlink.com>
2024-11-26 17:09:51 -0500

    testeeee

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,6 @@
-import json
-import pandas as pd
-
 def lambda_handler(event, context):
     my_list = [1, 2, 3, 4, 5, 6]
     print(my_list)
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': 200
     }
